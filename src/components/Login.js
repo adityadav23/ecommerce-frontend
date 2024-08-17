4045255fc5ce4ac40b657f4226ac2cdc24111def
Aditya Yadav <aditya023.ay@gmail.com>
2024-08-18 02:28:15 +0530

    update base

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,9 +6,10 @@ const Login = ({ onLogin }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-
+  const [submit, setSubmit] = useState(false);
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     console.log("Form submitted");
     try {
       const response = await api.post("/auth/login", { email, password });
@@ -17,31 +18,35 @@ const Login = ({ onLogin }) => {
     } catch (err) {
       console.error("Login Error:", err.response?.data || err.message);
       setError("Invalid credentials or server error");
+    } finally {
+      setSubmit(true);
     }
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Login</button>
-      </form>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-    </div>
+    !submit && (
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">Login</button>
+        </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </div>
+    )
   );
 };
 
